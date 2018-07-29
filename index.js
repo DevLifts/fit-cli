daf790fa839e4718d9c54c774a9c9fe62c58831a
J.C. Hiatt <jc@jchiatt.com>
2018-07-28 23:55:07 -0500

    install co, co-prompt. Test with example script from bitbucket tutorial

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-#!/usr/bin/env node
-console.log('Hello, world!');
+#!/usr/bin/env node --harmony
+var co = require('co');
+var prompt = require('co-prompt');
+var program = require('commander');
+
+program
+  .arguments('<file>')
+  .option('-u, --username <username>', 'The user to authenticate as')
+  .option('-p, --password <password>', 'The user\'s password')
+  .action(function(file) {
+    co(function *() {
+      var username = yield prompt('username: ');
+      var password = yield prompt.password('password: ');
+      console.log('user: %s pass: %s file: %s',
+      username, password, file);
+  })
+})
+  .parse(process.argv);
